5329510c0ac4ddaf0fa452d5a02f5376c4c46c45
muhammadpazli <mpazli65@gmail.com>
2019-05-22 05:06:28 +0700

    Commitpertama

---
+++ src/pazli/uas/desktop/pkg2019/koneksi.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pazli.uas.desktop.pkg2019;
+
+
+/**
+ *
+ * @author Petik
+ */
+public class koneksi {
+    private static Connection koneksi:
+}
